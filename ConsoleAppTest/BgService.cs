db6000123c8a7954c808b65543c076c01d26f500
master <ivicajelavic1@gmail.com>
2024-09-22 11:39:42 +0200

    ini sync

---
+++ ConsoleAppTest/BgService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    public class BgService : BackgroundService
+    {
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Ping");
+                await Task.Delay(4000);
+            }
+        }
+    }
+}
+
--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,2 +1,14 @@
 ﻿
+using ConsoleAppTest;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 Console.WriteLine("Hello, Ivica!");
+CreateHostBuilder(args).Build().Run();
+
+ static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .ConfigureServices((hostContext, services) =>
+               {
+                   services.AddHostedService<BgService>();
+               });
